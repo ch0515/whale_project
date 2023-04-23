2194581ab3560aa4f4f61bf4e7ed150279689d17
chaemin <w2114@e-mirim.hs.kr>
2023-04-23 21:00:19 +0900

    Revert "+ 게임 끝 화면으로 넘어가게 구현하기"
    This reverts commit 0415e49cae027d836882b9a60cc9e2723e887462.

--- main.h
+++ main.h
@@ -1,13 +1,25 @@
-#pragma warning(disable:4996)
-#include <errno.h>
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define MAX_STRING 70
-#define MAP_WIDTH 18
-#define MAP_HEIGHT 100
+#ifndef KEY_CODE
+#define KEY_CODE
 #define ONE_SECOND 10
 
-#define AUTH 0
+/*기본 함수*/
 
+void init();
+void gotoxy(int, int);
+void setColor(int, int);
+
+/*함수 선언*/
+void drawTitle();
+int menuDraw();
+int keyControl();
+void gloop();
+
+//키보드 값
 #define ESC 27
 #define ENTER 13
 #define UP 72
@@ -16,6 +28,53 @@
 #define RIGHT 77
 #define SPACEBAR 32
 
-void drawTitle();
-int menuDraw();
-void gloop();
+#endif
+
+#ifndef __COLOR_LIST_
+#define __COLOR_LIST_
+
+enum {
+	black,
+	blue,
+	green,
+	cyan,
+	red,
+	purple,
+	brown,
+	lightgray,
+	darkgray,
+	lightblue,
+	lightgreen,
+	lightcyan,
+	lightred,
+	lightpurple,
+	yellow,
+	white
+};
+
+#endif
+
+void init() {
+	system("mode con cols=120 lines=30 | title Whale game");
+
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE); //콘솔 핸들 가져오기
+	CONSOLE_CURSOR_INFO ConsoleCursor;
+	ConsoleCursor.bVisible = 0;
+	ConsoleCursor.dwSize = 1;
+	SetConsoleCursorInfo(consoleHandle, &ConsoleCursor);
+}
+
+void gotoxy(int x, int y) {
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);   //콘솔 핸들 가져오기
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(consoleHandle, pos);
+}
+
+//첫번쨰 : 텍스트, 두번쨰 : 배경
+void setColor(int forground, int background) {
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE); //콘솔 핸들 가져오기
+	int code = forground + background * 16;
+	SetConsoleTextAttribute(consoleHandle, code);
+}
